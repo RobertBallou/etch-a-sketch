031e4f8820b26f3074ed27e4cd59fe88cdeabf1b
Robert Ballou <havoc@ballou-pc.lan>
2022-07-30 14:22:11 -0700

    UI OVERHAUL - GRID SIZE - COLOR - RESET Added color picker, grid size slider, and reset button. Functions for those were added as well as event listeners and styling properties.

--- script.js
+++ script.js
@@ -1,29 +1,69 @@
 let container = document.getElementById('container');
-let gridSize = document.getElementById('grid-size')
+let pickColor = document.getElementById('pick-color')
+let sizeValue = document.getElementById('size-value')
+let sizeSlider = document.getElementById('size-slider')
 
-//create grid function that also attaches eventlistener
+let size = 16;
+let color = 'black';
+
+let mouseDown = false
+document.body.onmousedown = () => (mouseDown = true)
+document.body.onmouseup = () => (mouseDown = false)
+
+
+//start game with default size.
+createGrid(size);
+
+//create grid that also attaches eventlistener to grid as well as clears grid before making new grid.
 function createGrid(size){
-    if(size < (size*size)){
+    container.innerHTML = "";
+    if(size < 101){
         for (let i = 0; i < (size*size); i++){
             let div = document.createElement('div');
-            div.className = "grid-item"
+            div.className = "grid-item";
+            div.addEventListener('mouseover', changeColor)
+            div.addEventListener('mousedown', changeColor)
             container.appendChild(div);
         }
-        document.querySelectorAll('.grid-item').forEach(item => {
-            item.addEventListener('mouseover', event => {
-                item.classList.add('colored');
-                console.log(item);
-            })
-        })
+
+        //old way i did event listener
+        // document.querySelectorAll('.grid-item').forEach(item => {
+        //     item.addEventListener('mouseover', event => {
+        //         item.style.backgroundColor = color;
+        //         console.log(item);
+        //     })
+        // })
+
     }   
 }
 
-createGrid(16);
+//function for changing background color
+function changeColor(e){
+    console.log(e.type)
+    if(e.type === 'mouseover' && !mouseDown) return;
+    e.target.style.backgroundColor = color;
+}
 
-gridSize.addEventListener('click', event => {
-    let size = prompt("How many squares do you want per side of the grid? must be 100 or less");
-    container.style.gridTemplateRows = `repeat(${size}, 1fr`;
-    container.style.gridTemplateColumns = `repeat(${size}, 1fr`;
-    container.innerHTML = "";
+//event listener allowing you to clear hovered grid.
+document.getElementById('reset-grid').onclick = () => {
     createGrid(size);
-})
+}
+
+//pick a color for the background
+pickColor.oninput = (e) => {
+    color = e.target.value;
+    console.log(color);
+}
+
+//event listener to change the displayed size on slider
+sizeSlider.onmousemove = (e) => {
+    sizeValue.innerHTML = `${e.target.value} x ${e.target.value}`;
+}
+
+//event listener to change the grid to set size selected
+sizeSlider.onchange = (e) => {
+    size = e.target.value;
+    container.style.gridTemplateRows = `repeat(${e.target.value}, 1fr`;
+    container.style.gridTemplateColumns = `repeat(${e.target.value}, 1fr`;
+    createGrid(e.target.value);
+}
